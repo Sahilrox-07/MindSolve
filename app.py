b2385a1fb3edb28a4a15c4b443bd403bbcd014a5
Sahil <sahilrox528@gmail.com>
2026-03-29 19:38:51 +0530

    Ready for deployment

--- app.py
+++ app.py
@@ -123,4 +123,4 @@ def get_category():
 
 # 🚀 Run server
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
